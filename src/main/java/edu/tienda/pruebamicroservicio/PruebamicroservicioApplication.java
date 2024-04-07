2cea43573af248db7d569bd40add44fd9b5b98ae
Juan Renoj <jrenoj@gmail.com>
2024-04-07 15:39:22 -0600

    first commit

---
+++ src/main/java/edu/tienda/pruebamicroservicio/PruebamicroservicioApplication.java
@@ -0,0 +1,13 @@
+package edu.tienda.pruebamicroservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebamicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebamicroservicioApplication.class, args);
+	}
+
+}
---
+++ src/main/java/edu/tienda/pruebamicroservicio/entity/Person.java
@@ -0,0 +1,25 @@
+package edu.tienda.pruebamicroservicio.entity;
+
+import lombok.Data;
+
+
+public class Person {
+    int codigo;
+    String nombre;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
